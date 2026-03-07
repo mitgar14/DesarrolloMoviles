1b61ca87e4d7ea47adbfcab964a28e63b660aa99
Martín García <140340907+mitgar14@users.noreply.github.com>
2026-03-07 15:07:04 -0500

    feat: login component added over the app.tsx

--- ejercicio-1-pwa/src/App.tsx
+++ ejercicio-1-pwa/src/App.tsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
-
-
+import { useState } from 'react';
+import LoginForm from './components/LoginForm';
 
 import './App.css'
 
+interface Usuario {
+  email: string;
+  password: string;
+  nombre: string;
+  rol: string;
+}
+
 function App() {
+
+  const [usuario, setUsuario] = useState<Usuario | null>(() => {
+    const usuarioGuardado = localStorage.getItem("medicare_sesion")
+    return usuarioGuardado ? JSON.parse(usuarioGuardado) : null;
+  });
+
+  const login = (usuario: Usuario) => {
+    localStorage.setItem("medicare_sesion", JSON.stringify(usuario));
+    setUsuario(usuario);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("medicare_sesion");
+    setUsuario(null);
+  };
 
   return (
     <>
-
+      {!usuario ? (
+        <LoginForm onLogin={login} />
+      ) : (
+        <div className="app-container">
+          <p>Dashboard (usuario: {usuario.nombre}) <button onClick={logout}>Logout</button></p>
+        </div>
+      )}
     </>
   )
 }
