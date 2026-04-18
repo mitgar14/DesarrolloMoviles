ba3b3b1290a56414ec06dada0b2f2451ab500d4f
Martín García <140340907+mitgar14@users.noreply.github.com>
2026-04-18 13:57:44 -0500

    chore: borrando carpetas no relevantes

--- challenge-08/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'challenge-07',
-  webDir: 'dist'
-};
-
-export default config;
