9099896a3ebe057e47e5f7c5abd1538eb8eb8ac9
Martín García <140340907+mitgar14@users.noreply.github.com>
2026-04-25 14:45:29 -0500

    chore: delete old docs

--- parcial-2/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'parcial-2',
-  webDir: 'dist'
-};
-
-export default config;
